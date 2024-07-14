06271de46f5a599499b1f6dcc6245ab07b099c8f
basith4me <abdulbasith174xyz@gmail.com>
2024-07-14 20:02:08 +0530

    listed jobs for homepage and jobspage

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import jobs from "../jobs.json";
 import JobListing from "./JobListing";
 
-export default function JobListings() {
-
+export default function JobListings({ ishome = false }) {
   //used slice method to only show first three elements in the json array
-  const recentJobs = jobs.slice(0, 3);
+  const jobListing = ishome ? jobs.slice(0, 3) : jobs;
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          Browse Jobs
+          {ishome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {recentJobs.map((job) => (
+          {jobListing.map((job) => (
             <JobListing job={job} key={job.id} />
           ))}
         </div>
