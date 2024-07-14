0244fb782169f9b2d05fe2cadb7eaaaf893b1236
basith4me <abdulbasith174xyz@gmail.com>
2024-07-14 20:04:25 +0530

    homepage and jobs page done

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Hero from "../components/Hero";
+import HomeCard from "../components/HomeCard";
+import JobListings from "../components/JobListings";
+import ViewAllJobs from "../components/ViewAllJobs";
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      <HomeCard />
+      <JobListings ishome={true}/>
+      <ViewAllJobs />
+    </>
+  );
+}
