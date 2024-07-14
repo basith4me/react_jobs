68b869a379c5305b2dc4b50ebf3c3f4823b50dbd
basith4me <abdulbasith174xyz@gmail.com>
2024-07-14 20:00:51 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import React from "react";
-import NavBar from "./components/NavBar";
-import HomeCard from "./components/HomeCard";
-import Hero from "./components/Hero";
-import JobListings from "./components/JobListings";
-import ViewAllJobs from "./components/ViewAllJobs";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
+import JobsPage from "./pages/JobsPage";
+import NotFound from "./pages/NotFound";
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
 export default function App() {
-  return (
-    <>
-      <NavBar />
-      <Hero />
-      <HomeCard />
-      <JobListings />
-      <ViewAllJobs />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
